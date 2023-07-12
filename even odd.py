dc7e238ecc59df925bb6e3d8605e83d0f11233e3
kalyanigadade <gadadekalyani1@gmail.com>
2023-07-12 18:37:36 +0530

    FirstCommit

---
+++ even odd.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number:"))
+print("Value for user entered number is",n)
+if n%2==0:
+    print("Entered number is even",n)
+else:
+    print("Entered number is odd",n)
+
+
+
+
