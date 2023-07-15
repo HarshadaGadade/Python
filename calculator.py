3dd514a85476ee811e05a13c04d30cfcead8b55d
= <harshadagadade21@gmail.com>
2023-07-15 20:12:51 +0530

    basic code

--- calculator.py
+++ calculator.py
@@ -1,16 +1,38 @@
-def switch_case(case):
-    if case == 1:
-        print("Ninja 1")
-    elif case == 2:
-        print("Ninja 2")
-    elif case == 3:
-        print("Ninja 3")
+choice="y"
+def evenorodd(n):
+    if n % 2 == 0:
+        print("Entered number is even", n)
     else:
-        print("Invalid choice")
-
-
-switch_case(1)
-switch_case(2)
-switch_case(3)
-switch_case(4)
-
+        print("Entered number is odd", n)
+def primenumber(num1):
+    for i in range(2, num1):
+        if num1 % i == 0:
+            print("Not a prime Number")
+            break
+    else:
+        print("is a prime number")
+def calculator(num1,num2,ope):
+        if ope == "add":
+            ans=num1+num2
+            print("Sumation of two numbers is:",ans)
+        elif ope == "sub":
+            ans=num1-num2
+            print("Subtraction of two numbers is:",ans)
+        elif ope == "mul":
+            ans=num1*num2
+            print("Multiplication of two numbers is:",ans)
+        elif ope == "div":
+            ans=num1/num2
+            print("Division of two numbers is:",ans)
+        elif ope == "isevenodd":
+            evenorodd(num1)
+        elif ope == "primenumber":
+            primenumber(num1)
+        else:
+            print("Invalid choice")
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the second number:"))
+while choice=="y" or choice=="Y":
+    ch=input("Enter operation:")
+    calculator(n1,n2,ch)
+    choice=input("do u want to continue y/n:")
